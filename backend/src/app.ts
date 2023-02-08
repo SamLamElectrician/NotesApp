c53f716c3d4c69f3c8966f87765176febce493a8
Sammy <sammylam_1@hotmail.com>
2023-02-08 18:21:57 -0500

    mongoose schema in place

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,46 @@
 
 //for environment keys
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import NoteModel from './models/node';
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('hello world');
+//async in get request
+//ts knows types of req and res due to .get
+app.get('/', async (req, res) => {
+	try {
+		throw Error('Bazinga!');
+		//awaits the find operation
+		//.exec returns a promise
+		const notes = await NoteModel.find().exec();
+		//set response to 200 --> http for okay
+		//.json returns json file
+		res.status(200).json(notes);
+		//make sure read write is allowed for user
+	} catch (error) {
+		console.error;
+		let errorMessage = 'An unknown error occured';
+		//checking if the type is error
+		if (error instanceof Error) {
+			errorMessage = error.message;
+			//manually putting json, whereas notes is an array
+			res.status(500).json({ error: errorMessage });
+		}
+	}
 });
+
+// error handler kick in automatically, below normal endpoint
+// takes very specific argument, error,
+// set type here because .use type can be anything
+app.use(
+	(error: unknown, req: Request, res: Response, next: NextFunction) => {console.error;
+		let errorMessage = 'An unknown error occured';
+		//checking if the type is error
+		if (error instanceof Error) {
+			errorMessage = error.message;
+			//manually putting json, whereas notes is an array
+			res.status(500).json({ error: errorMessage })
+		})
 
 export default app;
---
+++ backend/src/models/node.ts
@@ -0,0 +1,22 @@
+// node model for data for mongoose
+// defines the schema for the db
+
+import { InferSchemaType, Schema, model } from 'mongoose';
+
+//how the data will be structured
+const noteSchema = new Schema(
+	{
+		//datatype has to be capitalize
+		title: { type: String, required: true },
+		text: { type: String },
+	},
+	//js has timestamps default, just needs to be after the main schema
+	{ timestamps: true }
+);
+//creating new type for ts
+//make sure note follows the schema of note Schema
+type Note = InferSchemaType<typeof noteSchema>;
+
+//name of collection
+//exported to use in code
+export default model<Note>('Note', noteSchema);
