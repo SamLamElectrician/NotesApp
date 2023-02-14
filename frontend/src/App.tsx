2bd320c34427786136279401c740311538dfa1d2
Sammy <sammylam_1@hotmail.com>
2023-02-14 18:08:16 -0500

    note interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Note } from './models/note';
 
 function App() {
+	const [notes, setNotes] = useState<Note[]>([]);
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>Samalama</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
 			</header>
 		</div>
 	);
---
+++ frontend/src/models/note.ts
@@ -0,0 +1,8 @@
+export interface Note {
+	//recieve string type of endpoint
+	_id: string;
+	title: string;
+	text?: string;
+	createdAt: string;
+	updatedAt: string;
+}
