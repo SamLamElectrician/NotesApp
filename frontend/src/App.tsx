63295afd00f3a6055cd739aa8af6db28fb1ea482
Sammy <sammylam_1@hotmail.com>
2023-02-15 16:53:33 -0500

    working on fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Note } from './models/note';
 
 function App() {
+	//empty array state
 	const [notes, setNotes] = useState<Note[]>([]);
+
+	//executes sideeffects outside render
+	useEffect(() => {
+		async function loadNotes() {
+			try {
+				//fetch call to get data
+				const response = await fetch('http://localhost:5000/api/notes', {
+					method: 'GET',
+				});
+				const notes = await response.json();
+			} catch (error) {
+				console.log(error);
+				alert(error);
+			}
+		}
+	});
+
 	return (
 		<div className='App'>
 			<header className='App-header'>
@@ -14,5 +32,6 @@ function App() {
 		</div>
 	);
 }
+//301
 
 export default App;
