608141bb142da89cf282db746ebc4e77906a8c7f
Sammy <sammylam_1@hotmail.com>
2023-02-16 18:43:56 -0500

    working on react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Note } from './models/note';
 
@@ -11,26 +10,23 @@ function App() {
 	useEffect(() => {
 		async function loadNotes() {
 			try {
-				//fetch call to get data
+				//fetch call to get data from back end
 				const response = await fetch('http://localhost:5000/api/notes', {
 					method: 'GET',
 				});
 				const notes = await response.json();
+				setNotes(notes);
 			} catch (error) {
 				console.log(error);
 				alert(error);
 			}
 		}
-	});
+		loadNotes();
+		//empty array dependency executes one time,
+		//no array means executes every render
+	}, []);
 
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Samalama</p>
-			</header>
-		</div>
-	);
+	return <div className='App'>{JSON.stringify(notes)}</div>;
 }
 //301
 
