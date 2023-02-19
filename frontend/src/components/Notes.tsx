4ee396d9209b0d55da4ea51c3278d6223614fdbc
Sammy <sammylam_1@hotmail.com>
2023-02-19 17:04:55 -0500

    making notes more readable

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Note.module.css';
 import { Card } from 'react-bootstrap';
 import { Note as NoteModel } from '../models/note';
+import { formatDate } from '../utils/formateDate';
 
 //typescript interface for the props
 interface NoteProps {
@@ -10,13 +11,19 @@ interface NoteProps {
 
 export default function Notes({ note, className }: NoteProps) {
 	const { title, text, createdAt, updatedAt } = note;
+	let createdUpdatedText: string;
+	if (updatedAt > createdAt) {
+		createdUpdatedText = 'Updated: ' + formatDate(updatedAt);
+	} else {
+		createdUpdatedText = 'Created: ' + formatDate(createdAt);
+	}
 	return (
 		<Card className={`${styles.noteCard} ${className}`}>
 			<Card.Body className={styles.cardBody}>
 				<Card.Title>{title}</Card.Title>
 				<Card.Text className={styles.cardText}>{text}</Card.Text>
 			</Card.Body>
-			<Card.Footer className='text-muted'>{createdAt}</Card.Footer>
+			<Card.Footer className='text-muted'>{createdUpdatedText}</Card.Footer>
 		</Card>
 	);
 }
--- frontend/src/utils/formateDate.ts
+++ frontend/src/utils/formateDate.ts
@@ -1,3 +1,4 @@
+//formating data to be more readable
 export function formatDate(dateString: string) {
 	return new Date(dateString).toLocaleDateString('en-US', {
 		year: 'numeric',
