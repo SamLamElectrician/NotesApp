4787d0e3c980f9e940592818894aa38272e7eafd
Sammy <sammylam_1@hotmail.com>
2023-02-24 17:30:15 -0500

    issue with ts and delete function

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -2,14 +2,21 @@ import styles from '../styles/Note.module.css';
 import { Card } from 'react-bootstrap';
 import { Note as NoteModel } from '../models/note';
 import { formatDate } from '../utils/formateDate';
+import { MdDelete } from 'react-icons/md';
+import stylesUtils from '../styles/utils.module.css';
 
 //typescript interface for the props
 interface NoteProps {
 	note: NoteModel;
+	onDeleteNoteClicked: (note: NoteModel) => void;
 	className?: string;
 }
 
-export default function Notes({ note, className }: NoteProps) {
+export default function Notes({
+	note,
+	onDeleteNoteClicked,
+	className,
+}: NoteProps) {
 	const { title, text, createdAt, updatedAt } = note;
 	let createdUpdatedText: string;
 	if (updatedAt > createdAt) {
@@ -20,8 +27,20 @@ export default function Notes({ note, className }: NoteProps) {
 	return (
 		<Card className={`${styles.noteCard} ${className}`}>
 			<Card.Body className={styles.cardBody}>
-				<Card.Title>{title}</Card.Title>
-				<Card.Text className={styles.cardText}>{text}</Card.Text>
+				<Card.Title className={stylesUtils.flexCenter}>
+					{title}
+					<MdDelete className='text-muted ms-auto'></MdDelete>
+				</Card.Title>
+				<Card.Text
+					className={styles.cardText}
+					onClick={(e: React.MouseEvent<HTMLElement>) => {
+						onDeleteNoteClicked(note);
+						//allows click to go through
+						e.stopPropagation();
+					}}
+				>
+					{text}
+				</Card.Text>
 			</Card.Body>
 			<Card.Footer className='text-muted'>{createdUpdatedText}</Card.Footer>
 		</Card>
