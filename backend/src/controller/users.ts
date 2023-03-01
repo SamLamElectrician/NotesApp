f73f5a3cdf1b72001c944ad462617c9f7cf6e124
Sammy <sammylam_1@hotmail.com>
2023-02-28 23:06:25 -0500

    checking login in backend

--- backend/src/controller/users.ts
+++ backend/src/controller/users.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import createHttpError from 'http-errors';
 import UserModel from '../models/user';
+import bcrypt from 'bcrypt';
 
 interface SignUpBody {
 	username?: string;
@@ -28,6 +29,33 @@ export const signUp: RequestHandler<
 		const existingUsername = await UserModel.findOne({
 			username: username,
 		}).exec();
+		//checks if there is an existing username within the database
+		if (existingUsername) {
+			throw createHttpError(
+				409,
+				'Username already taken. Please choose a different one or log in instead.'
+			);
+		}
+
+		const existingEmail = await UserModel.findOne({ email: email }).exec();
+
+		if (existingEmail) {
+			throw createHttpError(
+				409,
+				'A user with this email address already exists. Please log in instead.'
+			);
+		}
+		//hashing with bcrypt package
+		//salthash--> security measure to protect hash
+		const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+		const newUser = await UserModel.create({
+			username: username,
+			email: email,
+			password: passwordHashed,
+		});
+
+		res.status(201).json(newUser);
 	} catch (error) {
 		next(error);
 	}
