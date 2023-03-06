3b5b502da03e0e53714715b8879ca6ac8e0a2b6c
Sammy <sammylam_1@hotmail.com>
2023-03-06 15:31:39 -0500

    log in page and not log in page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,38 @@
 import { Container } from 'react-bootstrap';
 import styles from './styles/NotePage.module.css';
-
+import { useState, useEffect } from 'react';
 import LoginModal from './components/LoginModal';
 import NavBar from './components/NavBar';
 import SignUpModal from './components/SignUpModal';
-
+import { User } from './models/user';
+import * as NotesApi from './network/notes_api';
 function App() {
+	//checks if user is logged in following user interface or null
+	const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+	//controls sign up
+	const [showSignUpModal, setShowSignUpModal] = useState(false);
+	//controls login
+	const [showLoginpModal, setLoginModal] = useState(false);
+
+	useEffect(() => {
+		async function fetchLoggedInUser() {
+			try {
+				const user = await NotesApi.getLoggedInUser();
+				//if there is a user than it will update the use State
+				setLoggedInUser(user);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	});
+
 	return (
 		<div>
 			<NavBar
-				loggedInUser={null}
-				onLoginClicked={() => {}}
-				onSignUpClicked={() => {}}
-				onLogoutSuccessful={() => {}}
+				loggedInUser={loggedInUser}
+				onLoginClicked={() => setLoginModal(true)}
+				onSignUpClicked={() => setShowSignUpModal(true)}
+				onLogoutSuccessful={() => setLoggedInUser(null)}
 			></NavBar>
 			<Container className={styles.notesPage}>
 				{false && (
---
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotesPageLoggedOutView() {
+	return <p>Please log in to see your notes</p>;
+}
