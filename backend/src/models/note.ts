95fcc21312dd19f0b118c160f99cf8e8baddb8c1
Sammy <sammylam_1@hotmail.com>
2023-03-08 01:52:52 -0500

    working on individual accounts

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -1,11 +1,13 @@
 // node model for data for mongoose
 // defines the schema for the db
 
-import { InferSchemaType, Schema, model } from 'mongoose';
+import mongoose, { InferSchemaType, Schema, model } from 'mongoose';
 
 //how the data will be structured
 const noteSchema = new Schema(
 	{
+		//saves user id to each notes
+		userId: { type: Schema.Types.ObjectId, required: true },
 		//datatype has to be capitalize
 		title: { type: String, required: true },
 		text: { type: String },
